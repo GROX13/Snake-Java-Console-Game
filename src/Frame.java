9804cd26971dc544562beac80ee4290b823e7dca
GROX13 <giorgirokhadze@gmail.com>
2015-03-28 19:46:16 +0400

    Revert "Revert "Minor Changes.""
    This reverts commit 56e2e54734e2858c34a96bd74d05451a17a9bcbb.

--- src/Frame.java
+++ src/Frame.java
@@ -2,26 +2,29 @@ import javax.swing.JFrame;
 import java.awt.GridLayout;
 
 public class Frame extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
+
 	public Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Snake Game v1.00");
+		setTitle("Snake Game v1.01"); // :D
 		setResizable(false);
-		
+
 		init();
 	}
+
 	public void init() {
 		setLayout(new GridLayout(1, 1, 0, 0));
-	
-		Screen s = new Screen();
-		add(s);
-		
+
+		Screen screen = new Screen();
+		add(screen);
+
 		pack();
-		
+
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
+
 	public static void main(String[] args) {
 		new Frame();
 	}
